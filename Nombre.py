919d327ff070a514f758bd9a889234654b1427d3
marco23castro <55814968+marco23castro@users.noreply.github.com>
2019-09-25 19:25:16 -0500

    Create Nombre.py

---
+++ Nombre.py
@@ -0,0 +1,8 @@
+nombre=input("Nombre")
+#se representa como input nombre ya que diremos que se le asignara uno
+apellidos=input("Apellidos:")
+#aqui tambien
+nombreCompleto=nombre+" "+apellidos
+#aqui sumaremos o junatermos los dos nombres
+nombreCompleto=nombreCompleto.upper()
+print(nombreCompleto)
