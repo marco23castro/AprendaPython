44df2cf0e89cfe4f103e9bb2bdc65019a6a2625f
marco23castro <55814968+marco23castro@users.noreply.github.com>
2019-09-25 19:24:46 -0500

    Create Tablas.py

---
+++ Tablas.py
@@ -0,0 +1,10 @@
+for i in range(1,11):
+    encabezado="Tabla del {}"
+    print(encabezado.format(i))
+    print()
+    for j in range(1,11):
+        salida="{} x {} = {}"
+        print(salida.format(i,j,i*j))
+#al concluir con las iteraciones indicadas se ejecuta este codigo que es un salto
+        else:
+            print()
